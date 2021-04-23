a05965cf31e06aaf6c094d77f4e55634ce86f18d
chgomezz <82677974+chgomezz@users.noreply.github.com>
2021-04-23 18:20:08 -0500

    Update Read.py

--- Read.py
+++ Read.py
@@ -3,10 +3,20 @@
 import RPi.GPIO as GPIO
 from mfrc522 import simpleMFRC522
 
-reader = SimpleMFRC522()
-try: 
-        id, text= reader.read()
-        print(id)
-        print(text)
+obj = SimpleMFRC522()
+
+try:
+        leer = input('Nuevo Personal:')
+        print("Ponga su tarjeta para registrarse")
+        obj.write(leer)
+        print("Tarjeta leida")
 finally:
         GPIO.cleanup()
+        
+try: 
+        id, leer= obj.read()
+        print(id)
+        print(leer)
+finally:
+        GPIO.cleanup()
+        
