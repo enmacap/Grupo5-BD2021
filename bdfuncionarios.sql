b6dd2b8c8c714c80cda7b4e1164eddf22db1df7b
Jessbpd <jesspizarro@fiuna.edu.py>
2021-05-18 14:04:06 -0400

    Base de datos Funcionarios

---
+++ bdfuncionarios.sql
@@ -0,0 +1,64 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.4.18-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Volcando estructura para tabla bdfuncionarios.departamento
+CREATE TABLE IF NOT EXISTS `departamento` (
+  `id_departamento` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `infectados` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_departamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Volcando datos para la tabla bdfuncionarios.departamento: ~0 rows (aproximadamente)
+DELETE FROM `departamento`;
+/*!40000 ALTER TABLE `departamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `departamento` ENABLE KEYS */;
+
+-- Volcando estructura para tabla bdfuncionarios.funcionario
+CREATE TABLE IF NOT EXISTS `funcionario` (
+  `id_funcionario` int(11) NOT NULL,
+  `usuario` varchar(25) DEFAULT NULL,
+  `passwrd` varchar(25) DEFAULT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `correo` varchar(45) DEFAULT NULL,
+  `departamento_id` int(11) DEFAULT NULL,
+  `rfid_codigo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_funcionario`),
+  KEY `departamento_id` (`departamento_id`),
+  KEY `rfid_codigo` (`rfid_codigo`),
+  CONSTRAINT `funcionario_ibfk_1` FOREIGN KEY (`departamento_id`) REFERENCES `departamento` (`id_departamento`),
+  CONSTRAINT `funcionario_ibfk_2` FOREIGN KEY (`rfid_codigo`) REFERENCES `tarjeta` (`codigo_rfid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Volcando datos para la tabla bdfuncionarios.funcionario: ~0 rows (aproximadamente)
+DELETE FROM `funcionario`;
+/*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
+
+-- Volcando estructura para tabla bdfuncionarios.tarjeta
+CREATE TABLE IF NOT EXISTS `tarjeta` (
+  `codigo_rfid` int(11) NOT NULL,
+  PRIMARY KEY (`codigo_rfid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Volcando datos para la tabla bdfuncionarios.tarjeta: ~0 rows (aproximadamente)
+DELETE FROM `tarjeta`;
+/*!40000 ALTER TABLE `tarjeta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tarjeta` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
