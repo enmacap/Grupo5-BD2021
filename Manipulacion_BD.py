8c561fe8a41b08cfe090dcbf39180f27f3f6f9d5
chgomezz <82677974+chgomezz@users.noreply.github.com>
2021-05-19 20:53:18 -0400

    Update Manipulacion_BD.py

--- Manipulacion_BD.py
+++ Manipulacion_BD.py
@@ -7,42 +7,42 @@ class Manipulacion_BD:# con esta clae podemos acceder de diferentes formas a los
         passwd="admin", database="bdEjemploPy")
 
     def __str__(self):
-        datos=self.consulta_ciudades()
+        datos=self.consulta_empleado()
         aux=""
         for row in datos:
             aux=aux + str(row) + "\n"
         return aux
     
-    def consulta_ciudades(self):
+    def consulta_empleado(self):
         cur = self.cnn.cursor()
-        cur.execute("SELECT * FROM countries")
+        cur.execute("SELECT * FROM empleados")
         datos = cur.fetchall()
         cur.close()    
         return datos
 
-    def inserta_ciudad(self,ISO3, CountryName, Capital, CurrencyCode):
+    def inserta_empleado(self,ci, Name, ampellido, departamento):
         cur = self.cnn.cursor()
         sql='''INSERT INTO countries (ISO3, CountryName, Capital, CurrencyCode) 
-        VALUES('{}', '{}', '{}', '{}')'''.format(ISO3, CountryName, Capital, CurrencyCode)
+        VALUES('{}', '{}', '{}', '{}')'''.format(ci, Name, ampellido, departamento)
         cur.execute(sql)
         n=cur.rowcount
         self.cnn.commit()    
         cur.close()
         return n    
 
-    def elimina_ciudad(self,Id):
+    def elimina_empleado(self,Id):
         cur = self.cnn.cursor()
-        sql='''DELETE FROM countries WHERE Id = {}'''.format(Id)
+        sql='''DELETE FROM empleado WHERE Id = {}'''.format(Id)
         cur.execute(sql)
         n=cur.rowcount
         self.cnn.commit()    
         cur.close()
         return n   
 
-    def modifica_ciudad(self,Id, ISO3, CountryName, Capital, CurrencyCode):
+    def modifica_empleado(self,Id, ci, Name, ampellido, departamento):
         cur = self.cnn.cursor()
         sql='''UPDATE countries SET ISO3='{}', CountryName='{}', Capital='{}',
-        CurrencyCode='{}' WHERE Id={}'''.format(ISO3, CountryName, Capital, CurrencyCode,Id)
+        CurrencyCode='{}' WHERE Id={}'''.format(ci, Name, ampellido, departamento, Id)
         cur.execute(sql)
         n=cur.rowcount
         self.cnn.commit()    
